8b8fafa6c98b165308d6e7b75e5408ace186a452
LeHuyVuu <lehuyvuok@gmail.com>
2025-05-25 11:14:52 +0700

    push to vu

--- SWD392-backend/obj/Debug/net8.0/SWD392-backend.AssemblyInfo.cs
+++ SWD392-backend/obj/Debug/net8.0/SWD392-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWD392-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944b6f4ac1ff3b676a2a5edad151ff031cefe9a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWD392-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWD392-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
