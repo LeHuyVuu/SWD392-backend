21efdd88c3450b4bab827a36c33f88e9aab00006
LeHuyVuu <lehuyvuok@gmail.com>
2025-05-30 15:09:02 +0700

    model

---
+++ SWD392-backend/Models/Response/OrderDetailResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace SWD392_backend.Models.Response;
+
+public class OrderDetailResponse
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+    public double DiscountPercent { get; set; }
+    public string Note { get; set; } = null!;
+}
