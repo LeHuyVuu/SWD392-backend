dff207636ac3eae01fb44a86838a79155e68c7f0
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-06-02 23:31:30 +0700

    update fields product

--- SWD392-backend/Models/Response/ProductResponse.cs
+++ SWD392-backend/Models/Response/ProductResponse.cs
@@ -6,10 +6,12 @@
         public string Name { get; set; } = null!;
         public double Price { get; set; }
         public double DiscountPrice { get; set; }
+        public double DiscountPercent { get; set; }
         public string Slug { get; set; }
         public double RatingAverage { get; set; }
         public bool IsSale { get; set; }
         public int StockInQuantity { get; set; }
+        public int SoldQuantity { get; set; }
         public string ImageUrl { get; set; }
         public int CategoriesId { get; set; }
         public int SupplierId { get; set; }
