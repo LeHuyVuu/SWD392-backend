537f5c9cc5231ef1fa738201ef845583bcdb338e
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-06-03 22:03:19 +0700

    udpate summary for product endpoint

--- SWD392-backend/Infrastructure/Controllers/UploadController.cs
+++ SWD392-backend/Infrastructure/Controllers/UploadController.cs
@@ -22,6 +22,18 @@ namespace SWD392_backend.Infrastructure.Controllers
             _uploadService = uploadService;
         }
 
+        /// <summary>
+        /// Tạo các URL tạm thời (presigned URLs) để tải lên nhiều hình ảnh sản phẩm.
+        /// </summary>
+        /// <param name="request">Đối tượng chứa thông tin yêu cầu tải lên hình ảnh, bao gồm CategoryId, ProductSlug, ProductId, SupplierId và ContentTypes.</param>
+        /// <returns>
+        /// - 200 OK: Trả về UploadMultipleProductImgsResponse chứa danh sách các URL tạm thời và thông tin hình ảnh.
+        /// - 400 Bad Request: Nếu yêu cầu không hợp lệ (thiếu thông tin hoặc ContentTypes rỗng).
+        /// - 500 Internal Server Error: Nếu xảy ra lỗi server (ví dụ: không lấy được slug danh mục hoặc lỗi dịch vụ S3).
+        /// </returns>
+        /// <remarks>
+        /// Tạo presigned URLs cho hình ảnh sản phẩm dựa trên danh mục, sản phẩm và nhà cung cấp. Mỗi URL được tạo kèm key và liên kết CDN (hình đầu tiên sẽ là hình chính cho product).
+        /// </remarks>
         [HttpPost("upload-images")]
         public async Task<ActionResult<UploadProductImgResponse>> GetPresignedUrl([FromBody] UploadProductImgsRequest request)
         {
@@ -34,6 +46,19 @@ namespace SWD392_backend.Infrastructure.Controllers
             return Ok(HTTPResponse<object>.Response(200, "Successfully", upload));
         }
 
+        /// <summary>
+        /// Gửi lại link các hình ảnh nhận được từ endpoint upload-images
+        /// </summary>
+        /// <param name="id">ID của sản phẩm (kiểu số nguyên).</param>
+        /// <param name="imageUrl">Danh sách URL của các hình ảnh đã tải lên.</param>
+        /// <returns>
+        /// - 204 No Content: Nếu xác nhận và liên kết hình ảnh thành công.
+        /// - 400 Bad Request: Nếu ID không hợp lệ, danh sách URL rỗng, hoặc xác nhận thất bại.
+        /// - 500 Internal Server Error: Nếu xảy ra lỗi server.
+        /// </returns>
+        /// <remarks>
+        /// Xác nhận các hình ảnh đã tải lên, lưu vào cơ sở dữ liệu, và cập nhật sản phẩm trong Elasticsearch. (link đầu tiên sẽ là hình chính cho product)
+        /// </remarks>
         [HttpPost("{id:int}/confirm-upload")]
         public async Task<IActionResult> ConfirmUploadImage(int id, [FromBody] List<string> imageUrl)
         {
